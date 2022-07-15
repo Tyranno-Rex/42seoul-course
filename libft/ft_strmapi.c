c283ee088db17ec6172094d08a7a8c681544369d
unknown <80687043+jeongeunseong-khlug@users.noreply.github.com>
2022-07-15 20:18:49 +0900

    42

---
+++ libft/ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/15 20:03:24 by marvin            #+#    #+#             */
+/*   Updated: 2022/07/15 20:03:24 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char				*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t		idx;
+	char		*tmp;
+
+	if (!s || !f ||
+			!(tmp = (char *)malloc(sizeof(char) * (ft_strlen((char *)s) + 1))))
+		return (NULL);
+	idx = 0;
+	while (idx < (ft_strlen((char *)s)))
+	{
+		tmp[idx] = f(idx, s[idx]);
+		idx++;
+	}
+	tmp[idx] = '\0';
+	return (tmp);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -34,6 +34,7 @@ size_t				ft_strlen(const char *str);
 
 
 int					ft_atoi(char *the_char);
+void				*ft_bzero (void *s, size_t n);
 void				*ft_calloc(size_t nmemb, size_t size);
 int					ft_isalnum(int c);
 int					ft_isalpha(int a);
@@ -42,7 +43,10 @@ int					ft_isdigit(int c);
 int					ft_isprint(int c);
 void				*ft_memchr(const void *s, int c, size_t n);
 int					ft_memcmp(const void *base1, const void *base2, size_t n);
+void				*ft_memcpy(void *dest, const void *src, size_t n);
+void				*ft_memmove(void *dest, const void *src, size_t n);
 void				*ft_memset(void *s, int c, size_t n);
+char				**ft_split(char *str, char *charset);
 char				*ft_strchr(const char *str, int c);
 char				*ft_strdup(char *src);
 char				*ft_strjoin(char const *s1, char const *s2);
@@ -53,9 +57,6 @@ char				*ft_strtrim(char const *s1, char const *set);
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 int					ft_tolower(int c);
 int					ft_toupper(int c);
-void				*ft_bzero (void *s, size_t n);
-void				*ft_memcpy(void *dest, const void *src, size_t n);
-void				*ft_memmove(void *dest, const void *src, size_t n);
 
 
 
@@ -65,7 +66,6 @@ void				*ft_memmove(void *dest, const void *src, size_t n);
 
 
 void				*ft_memccpy(void *dest, const void *src, int c, size_t n);
-char				**ft_split(char const *s, char c);
 char				*ft_itoa(int n);
 char				*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void				ft_putchar_fd(char c, int fd);
