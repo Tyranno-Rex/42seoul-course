846b55fb4ccc5b96f367ab740a69cb0a58124489
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-01-31 15:58:35 +0900

    첫 헤더 파일 작성 및 연습

---
+++ include/push_swap.h
@@ -0,0 +1,26 @@
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#ifdef _WIN32
+	#include <io.h>
+#else
+	#include <unistd.h>
+#endif
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <limits.h>
+
+typedef struct s_push_swap
+{
+	int				value;
+	int				index;
+	int				pos;
+	int				target_pos;
+	int				cost_a;
+	int				cost_b;
+	struct s_push_swap	*next;
+}	t_push_swap;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,29 @@
+#include "./../include/push_swap.h"
+
+
+int	ft_check_ac_av(int ac, char **av)
+{
+	if (ac < 2)
+	{
+		write(1, "ac is must be more than 2");
+		return (0);
+	}
+	while (av)
+	{
+		/* code */
+	}
+	
+	return (1)
+}
+
+
+
+int main(int ac, char **av)
+{
+    int			stack_len;
+    t_push_swap *stack_a;
+    t_push_swap *stack_b;
+
+	if (ft_check_ac_av(ac, av) != 1)
+		return (0);
+}
