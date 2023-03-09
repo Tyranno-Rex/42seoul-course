4c39992370c318177f08ba358ea296b2ee7c2253
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-03-09 17:52:09 +0900

    1단계 묹제들 전부 연습 완료

--- inter/inter.c
+++ inter/inter.c
@@ -1,25 +1,25 @@
 #include <unistd.h>
 
-int	main(int ac, char *av[])
+int main(int argc, char **argv)
 {
 	int			i;
 	int			j;
-	static int	tab[256];
-    
-	if (ac == 3)
+	static int	str[256];
+
+	if (argc == 3)
 	{
 		i = 0;
-		while (av[1][i])
+		while (argv[1][i])
 		{
 			j = 0;
-			while (av[2][j])
+			while (argv[2][j])
 			{
-				if (av[1][i] == av[2][j])
+				if (argv[1][i] == argv[2][j])
 				{
-					if (tab[(int)av[1][i]] == 0)
+					if (str[(int)argv[1][i]] == 0)
 					{
-						write(1, &av[1][i], 1);
-						tab[(int)av[1][i]] = 1;
+						write(1, &argv[1][i], 1);
+						str[(int)argv[1][i]] = 1;
 					}
 				}
 				j++;
@@ -30,3 +30,34 @@ int	main(int ac, char *av[])
 	write(1, "\n", 1);
 	return (0);
 }
+
+// int	main(int ac, char *av[])
+// {
+// 	int			i;
+// 	int			j;
+// 	static int	tab[256];
+    
+// 	if (ac == 3)
+// 	{
+// 		i = 0;
+// 		while (av[1][i])
+// 		{
+// 			j = 0;
+// 			while (av[2][j])
+// 			{
+// 				if (av[1][i] == av[2][j])
+// 				{
+// 					if (tab[(int)av[1][i]] == 0)
+// 					{
+// 						write(1, &av[1][i], 1);
+// 						tab[(int)av[1][i]] = 1;
+// 					}
+// 				}
+// 				j++;
+// 			}
+// 			i++;
+// 		}
+// 	}
+// 	write(1, "\n", 1);
+// 	return (0);
+// }
--- union/union.c
+++ union/union.c
@@ -1,39 +1,59 @@
 #include <unistd.h>
 
 
-int main(int argc, int **argv)
+int main(int argc, char **argv)
 {
-	int		i = 0;
-	int 	j = 0;
+	int 		i;
+	int 		j;
+	static int 	str[256];
 	
-}
-
-
-
-int	main(int ac, char*av[])
-{
-	int			i;
-	int			j;
-	static int	tab[256];
-
-	if (ac == 3)
+	if (argc == 3)
 	{
-		j = 1;
-		while (j <= 2)
+		i = 1;
+		while (i <= 2)
 		{
-			i = 0;
-			while (av[j][i])
+			j = 0;
+			while (argv[i][j])
 			{
-				if (tab[(int)av[j][i]] == 0)
+				if (str[(int)argv[i][j]] == 0)
 				{
-					write(1, &av[j][i], 1);
-					tab[(int)av[j][i]] = 1;
+					write(1, &argv[i][j], 1);
+					str[(int)argv[i][j]] = 1;
 				}
-				i++;
+				j++;
 			}
-			j++;
+			i++;
 		}
 	}
 	write(1, "\n", 1);
-	return (0);
+	return 0;
 }
+
+
+// int	main(int ac, char*av[])
+// {
+// 	int			i;
+// 	int			j;
+// 	static int	tab[256];
+
+// 	if (ac == 3)
+// 	{
+// 		j = 1;
+// 		while (j <= 2)
+// 		{
+// 			i = 0;
+// 			while (av[j][i])
+// 			{
+// 				if (tab[(int)av[j][i]] == 0)
+// 				{
+// 					write(1, &av[j][i], 1);
+// 					tab[(int)av[j][i]] = 1;
+// 				}
+// 				i++;
+// 			}
+// 			j++;
+// 		}
+// 	}
+// 	write(1, "\n", 1);
+// 	return (0);
+// }
