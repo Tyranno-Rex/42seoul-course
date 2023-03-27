01efb113b9cfe5e24f0b51aef7872ffc07cf6f0f
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-03-27 11:34:34 +0900

    4번째 테스트

---
+++ gnl/test4.c
@@ -0,0 +1,32 @@
+#include "test4.h"
+char *get_next_line(int fd)
+{
+    int i = 0;
+    int rd;
+    char character;
+    char *buffer = malloc(1000000);
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (free(buffer), NULL);
+    while ((rd = read(fd, &character, 1)) > 0)
+    {
+        buffer[i++] = character;
+        if (character == '\n')
+            break;
+    }
+    if ((!rd && !buffer[i - 1]) || rd == -1)
+        return (free(buffer), NULL);
+    buffer[i] = '\0';
+    return (buffer);
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+int main()
+{
+    int fd;
+    char *line;
+    fd = open("text.txt", O_RDONLY);
+    while ((line = get_next_line(fd)) != NULL)
+        printf("%s", line);
+    return (0);
+}
