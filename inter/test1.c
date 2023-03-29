d1283abfca6c39c5a1b5950ee008fa3480bc75dd
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-03-29 18:21:26 +0900

    시발 뭔데 진짜

--- inter/test1.c
+++ inter/test1.c
@@ -1,31 +1,13 @@
 #include <unistd.h>
 
-
 int main(int argc, char **argv)
 {
-    int         i;
-    int         j;
-    static char str[256];
+    int i;
+    int j;
 
-    if (argc == 3)
+    while (argv)
     {
-        i = 0;
-        while (argv[1][i])
-        {
-            j = 0;
-            while (argv[2][j])
-            {
-                if (argv[1][i] == argv[2][j])
-                {
-                    if (str[(int)argv[1][i]] == 0)
-                        write(1, &argv[1][i], 1);
-                        str[(int)argv[1][i]] = 1;
-                }
-                j++;
-            }
-            i++;
-        }
+        /* code */
     }
-    write(1, "\n", 1);
-    return (0);
+    
 }
